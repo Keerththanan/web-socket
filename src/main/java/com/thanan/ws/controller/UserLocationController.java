33f85d6829c7b23725c1d93b64c214874b3483aa
keerththanan-dilimatrix <kerthy92@yahoo.com>
2024-02-05 15:27:41 +0530

    Web socket implementation

--- src/main/java/com/thanan/ws/controller/UserLocationController.java
+++ src/main/java/com/thanan/ws/controller/UserLocationController.java
@@ -1,14 +1,15 @@
 package com.thanan.ws.controller;
 
-import com.thanan.ws.entity.UserLocation;
 import com.thanan.ws.exception.UserLocationNotFoundException;
 import com.thanan.ws.model.request.UserLocationRequest;
 import com.thanan.ws.model.response.UserLocationResponse;
 import com.thanan.ws.service.UserLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -56,5 +55,13 @@ public class UserLocationController {
         } catch (Exception ex) {
             throw new Exception(ex);
         }
+    }
+
+    @MessageMapping("/{user_id}/user-locations")
+    @SendTo("/topic/{user_id}/user-locations")
+    public UserLocationResponse sendUserLocation(@DestinationVariable Integer user_id,
+                                                 @Payload UserLocationRequest userLocationRequest) throws Exception {
+        userLocationService.addUserLocation(userLocationRequest);
+        return userLocationService.getCurrentLocation(user_id);
     }
 }
